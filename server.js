9c90796fc18f53df1a1383ec0dd5d7aa9333f521
Tiffany Conroy <tiffany.conroy@gmail.com>
2011-11-06 17:47:52 +0100

    my preferred formatting for if else blocks and function declaration

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-var
-  url = require('url'),
-  fs = require('fs'),
-  http = require('http'),
-  bee = require('beeline'),
-  SC_CLIENT_ID = '1288146c708a6fa789f74748fe960337';
+var url = require('url'),
+    fs = require('fs'),
+    http = require('http'),
+    bee = require('beeline'),
+    SC_CLIENT_ID = '1288146c708a6fa789f74748fe960337';
 
-var errorHandler = function(err, res) {
+var errorHandler = function (err, res) {
   console.log(err);
   res.writeHead(500);
   res.end();
 };
 
-var scResolve = function(resource, finalResponse, callback) {
+var scResolve = function (resource, finalResponse, callback) {
   var reqOptions = {
     host: 'api.soundcloud.com',
     port: 80,
@@ -22,16 +21,16 @@ var scResolve = function(resource, finalResponse, callback) {
     }
   };
 
-  return http.request(reqOptions, function(res) {
+  return http.request(reqOptions, function (res) {
     if (!res.headers.location) {
       res.headers['Content-Length'] = 0;
       finalResponse.writeHead(res.statusCode, res.headers);
       finalResponse.end();
-    }
-    else
+    } else {
       callback && callback(res);
+    }
   })
-  .on('error', function(err) {
+  .on('error', function (err) {
     errorHandler(err, finalResponse);
   })
   .end();
@@ -48,18 +47,19 @@ var router = bee.route({
     '.js': 'text/javascript'
   }),
 
-  'r`^/(index(\\.html?)?)?(\\?.*)?$`': function(req, finalResponse, matches) {
-    return fs.readFile('./public/index.html', function(err, data) {
-      if (err) console.log(err);
-      else {
+  'r`^/(index(\\.html?)?)?(\\?.*)?$`': function (req, finalResponse, matches) {
+    return fs.readFile('./public/index.html', function (err, data) {
+      if (err) {
+        console.log(err);
+      } else {
         finalResponse.writeHead(200, { 'Content-Type': 'text/html' });
         finalResponse.end(data);
       }
     });
   },
 
-  'r`^/([\\w-_]+)/([\\w-_]+)/audio`': function(req, finalResponse, matches) {
-    scResolve(matches.join('/'), finalResponse, function(res) {
+  'r`^/([\\w-_]+)/([\\w-_]+)/audio`': function (req, finalResponse, matches) {
+    scResolve(matches.join('/'), finalResponse, function (res) {
       var reqOptions = url.parse(res.headers.location);
       reqOptions = {
         host: reqOptions.host,
@@ -70,13 +70,13 @@ var router = bee.route({
         }
       };
 
-      http.get(reqOptions, function(res) {
+      http.get(reqOptions, function (res) {
         res.setEncoding('utf-8');
         var track = '';
-        res.on('data', function(chunk) {
+        res.on('data', function (chunk) {
           track += chunk;
         })
-        .on('end', function() {
+        .on('end', function () {
           track = JSON.parse(track);
           reqOptions = url.parse(track.stream_url);
           reqOptions = {
@@ -87,13 +87,12 @@ var router = bee.route({
             }
           };
 
-          http.get(reqOptions, function(res) {
+          http.get(reqOptions, function (res) {
             if (!res.headers.location) {
               res.headers['Content-Length'] = 0;
               finalResponse.writeHead(404, { 'Content-Type': 'application/octet-stream' });
               finalResponse.end();
-            }
-            else {
+            } else {
               res.setEncoding('binary');
               reqOptions = url.parse(res.headers.location);
               reqOptions = {
@@ -102,43 +101,43 @@ var router = bee.route({
                 headers: {
                   'User-Agent': 'AudioJedit'
                 }
-
               };
 
-              http.get(reqOptions, function(res) {
+              http.get(reqOptions, function (res) {
                 finalResponse.writeHead(res.statusCode, res.headers);
 
-                res.on('data', function(chunk) {
+                res.on('data', function (chunk) {
                   finalResponse.write(chunk);
                 })
-                .on('end', function() {
+                .on('end', function () {
                   finalResponse.end();
                 });
               })
-              .on('error', function(err) {
+              .on('error', function (err) {
                 errorHandler(err, finalResponse);
               })
             }
           })
-          .on('error', function(err) {
+          .on('error', function (err) {
             errorHandler(err, finalResponse);
           })
         })
       })
-      .on('error', function(err) {
+      .on('error', function (err) {
         errorHandler(err, finalResponse);
       })
     });
   },
 
-  'r`^/([\\w-_]+)/([\\w-_]+)(\\.\\w+)?`': function(req, finalResponse, matches) {
+  'r`^/([\\w-_]+)/([\\w-_]+)(\\.\\w+)?`': function (req, finalResponse, matches) {
     var format = matches.filter(Boolean).length == 3 ? matches.pop().substring(1) : 'html';
 
-    scResolve(matches.join('/'), finalResponse, function(res) {
+    scResolve(matches.join('/'), finalResponse, function (res) {
       if (format == 'html') {
-        fs.readFile('./public/index.html', function(err, data) {
-          if (err) console.log(err);
-          else {
+        fs.readFile('./public/index.html', function (err, data) {
+          if (err) {
+            console.log(err);
+          } else {
             finalResponse.writeHead(200, { 'Content-Type': 'text/html' });
             finalResponse.end(data);
           }
@@ -155,16 +154,16 @@ var router = bee.route({
           }
         };
 
-        http.get(reqOptions, function(res) {
+        http.get(reqOptions, function (res) {
           finalResponse.writeHead(res.statusCode, res.headers);
-          res.on('data', function(chunk) {
+          res.on('data', function (chunk) {
             finalResponse.write(chunk);
           })
-          .on('end', function() {
+          .on('end', function () {
             finalResponse.end();
           });
         })
-        .on('error', function(err) {
+        .on('error', function (err) {
           errorHandler(err, finalResponse);
         })
       }
